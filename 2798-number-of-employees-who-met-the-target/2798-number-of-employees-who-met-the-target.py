d0ed3d4378da9c0758c00f30f0daf95731532e97
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2023-12-10 14:57:57 +0300

    Time: 20 ms (79.17%), Space: 13.2 MB (76.80%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def numberOfEmployeesWhoMetTarget(self, hours, target):
-        result = 0
+        counter = 0
         for i in range(len(hours)):
             if hours[i] >= target:
-                result += 1
-        return result
+                counter +=1
+        return counter
         
