4c6e15ceeb0828ec9d7fcca39a52d5dbe5d20961
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2023-12-10 13:09:00 +0300

    Time: 15 ms (44.02%), Space: 13.4 MB (16.18%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.py
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -2,7 +2,7 @@ class Solution(object):
     def convertTemperature(self, celsius):
         result = []
         kelvin = celsius + 273.15
-        fahrenheit = (celsius * 1.80) + 32.00
+        fahrenheit = celsius * 1.80 + 32.00
         result.append(kelvin)
         result.append(fahrenheit)
         return result
