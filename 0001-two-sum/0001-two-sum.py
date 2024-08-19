8aa4c803402ca3ab0ec241c01fdbd538c5b62de5
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2024-08-19 21:18:21 +0300

    Time: 28 ms (97.94%), Space: 12.6 MB (41.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,11 +4,10 @@ class Solution(object):
         result = []
         for num in range(len(nums)):
             if target-nums[num] in target_map:
-                result.append(nums.index(target-nums[num]))
+                result.append(target_map[target-nums[num]])
                 result.append(num)
                 return result
-            elif nums[num] not in target_map:
-                target_map[nums[num]] = 1
-            else:
-                target_map[nums[num]] += 1
+            if nums[num] not in target_map:
+                target_map[nums[num]] = num
+
         
