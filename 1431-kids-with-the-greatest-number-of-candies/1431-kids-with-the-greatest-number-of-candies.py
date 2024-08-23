eaafc33b6eb7c67256429299ee4a4d5a224e8a0d
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2024-08-23 14:55:31 +0300

    Time: 19 ms (61.25%), Space: 11.8 MB (8.41%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -2,10 +2,7 @@ class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
         if len(candies)<2:
             return [True]
-        max_val = candies[0]
-        for can in candies:
-            if can > max_val:
-                max_val = can
+        max_val = max(candies)
         for can in range(len(candies)):
             if candies[can] + extraCandies >= max_val:
                 candies[can] = True
