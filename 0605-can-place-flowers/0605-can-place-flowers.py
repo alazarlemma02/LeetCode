33632e8bce108c89ce1c21d0c5548bd533a91d4d
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2024-08-23 17:23:20 +0300

    Time: 123 ms (24.54%), Space: 12.3 MB (57.66%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def canPlaceFlowers(self, flowerbed, n):
         if n==0: return True
-        m_flowerbed = [0] + flowerbed + [0]
-        for flower in range(1, len(m_flowerbed)-1):
-            if m_flowerbed[flower-1]==0 and m_flowerbed[flower+1]==0 and m_flowerbed[flower]==0:
-                m_flowerbed[flower] = 1
+        for flower in range(len(flowerbed)):
+            if flowerbed[flower]==0 and (flower==0 or flowerbed[flower-1]==0) and (flower==len(flowerbed)-1 or flowerbed[flower+1]==0):
+                flowerbed[flower]=1
                 n-=1
                 if n==0: return True
         return False
