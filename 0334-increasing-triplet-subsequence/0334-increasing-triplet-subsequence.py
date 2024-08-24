749b2ff8c4dee3cf0984fefd0e38925113b20845
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2024-08-24 16:52:39 +0300

    Time: 845 ms (31.86%), Space: 38.5 MB (20.13%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,17 +1,11 @@
 class Solution(object):
     def increasingTriplet(self, nums):
         if len(nums) < 2: return False
-        left_min = [nums[0]]
-        right_max = [nums[-1]]
-        
-        for i in range(1,len(nums)):
-            left_min.append(min(left_min[-1], nums[i]))
-            
-        for i in range(1,len(nums)):
-            right_max.append(max(right_max[-1], nums[len(nums)-i-1]))
-            
-        right_max.reverse()
+        fs = float("inf")
+        sc = float("inf")
         for i in range(len(nums)):
-            if left_min[i]<nums[i]<right_max[i]: return True
+            if nums[i] <= fs: fs = nums[i]
+            elif nums[i] <= sc: sc = nums[i]
+            else: return True
         return False
         
