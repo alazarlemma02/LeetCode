0be7a111ef576ae3d36ebf6074778f7837a34973
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2024-08-24 12:20:59 +0300

    Time: 16 ms (58.17%), Space: 12 MB (26.15%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,17 +1,5 @@
 class Solution(object):
     def reverseWords(self, s):
         s = s.split()
-        s.reverse()
-        res = ""
-        words = []
-        for word in s:
-            if word !="":
-                words.append(word)
-        i = 0
-        while i < len(words):
-            res+=words[i]
-            if i != len(words)-1:
-                res+=" "
-            i += 1
-        return res
+        return " ".join(reversed(s))
         
