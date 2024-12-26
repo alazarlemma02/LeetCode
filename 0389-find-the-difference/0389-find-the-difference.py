819307cc04e00904633fdeb568b55f20986a2ac8
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2024-12-26 09:10:06 +0300

    Time: 0 ms (100.00%), Space: 12.4 MB (8.64%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def findTheDifference(self, s, t):
-        MAP = {}
+        # MAP = {}
         for i in range(len(t)):
-            if t[i] not in MAP:
-                MAP[t[i]] = [s.count(t[i]), t.count(t[i])]
-        for val in MAP:
-            if MAP[val][0] != MAP[val][1]:
-                return val
+            if s.count(t[i]) != t.count(t[i]):
+                return t[i]
+        #         MAP[t[i]] = [s.count(t[i]), t.count(t[i])]
+        # for val in MAP:
+        #     if MAP[val][0] != MAP[val][1]:
+        #         return val
